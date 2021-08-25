21abec2610996dc278b47061caf58f1927aa8e0e
mahmood <mahmoodimtiaz.786@gmail.com>
2021-08-25 09:01:55 +0600

    SignUp and Login Screen

---
+++ app/src/main/java/com/mahmood_imtiaz/quizapp/Activity/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.mahmood_imtiaz.quizapp.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mahmood_imtiaz.quizapp.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
---
+++ app/src/main/java/com/mahmood_imtiaz/quizapp/LoginIntro.kt
@@ -0,0 +1,11 @@
+package com.mahmood_imtiaz.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginIntro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_intro)
+    }
+}
